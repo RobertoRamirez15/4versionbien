e33013f77b64d3e7ed3cebf7339f63cc456d8af5
RobertoRamirez15 <damcb164e29@gmail.com>
2015-05-22 13:06:49 -0500

    4.1version
    tercera subida

--- js/acciones.js
+++ js/acciones.js
@@ -4,9 +4,9 @@ $(document).ready(function(e){
      document.addEventListener("deviceready",function(){
         $('#disp table td').eq(3).text(device.model);
         $('#disp table td').eq(5).text(device.cordova);
-        $('#disp table td').eq(7).text(device.plataform);
+        $('#disp table td').eq(7).text(device.platform);
         $('#disp table td').eq(9).text(device.version);
-        $('#disp table td').eq(11).text(device.uiid);
+        $('#disp table td').eq(11).text(device.uuid);
      document.addEventListener("pause", function(){
           escribehistoria('la app se pauso');
      },false);//pause
